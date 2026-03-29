6df54a742e89f21d50d7684696fc81852a3df933
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-03-29 17:10:34 -0300

    salvamento de filmes no localStorage

--- cinema/js/script.js
+++ cinema/js/script.js
@@ -2,7 +2,29 @@ const btnSalvarFilme = document.getElementById("btnSalvarFilme");
 
 if (btnSalvarFilme) {
     btnSalvarFilme.addEventListener("click", function () {
-        console.log("Clicou em Salvar Filme")
+
+        const titulo = document.getElementById("titulo").value;
+        const genero = document.getElementById("genero").value;
+        const descricao = document.getElementById("descricao").value;
+        const classificacao = document.getElementById("classificacao").value;
+        const duracao = Number(document.getElementById("duracao").value);
+        const dataEstreia = document.getElementById("dataEstreia").value;
+
+        const filme = {
+            titulo,
+            genero,
+            descricao,
+            classificacao,
+            duracao,
+            dataEstreia
+        };
+
+        let filmes = JSON.parse(localStorage.getItem("filmes")) || [];
+
+        filmes.push(filme)
+
+        localStorage.setItem("filmes", JSON.stringify(filmes))
 
     });
+
 }
