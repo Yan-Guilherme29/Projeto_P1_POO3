16a6ab8ab16d266d87af68ab2f495d314880022e
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-03-29 20:11:37 -0300

    validação completa e feedback no cadastro de filmes

--- cinema/js/script.js
+++ cinema/js/script.js
@@ -1,14 +1,30 @@
 const btnSalvarFilme = document.getElementById("btnSalvarFilme");
+const mensagem = document.getElementById("mensagem")
 
 if (btnSalvarFilme) {
     btnSalvarFilme.addEventListener("click", function () {
 
-        const titulo = document.getElementById("titulo").value;
-        const genero = document.getElementById("genero").value;
-        const descricao = document.getElementById("descricao").value;
+        const titulo = document.getElementById("titulo").value.trim();
+        const genero = document.getElementById("genero").value.trim();
+        const descricao = document.getElementById("descricao").value.trim();
         const classificacao = document.getElementById("classificacao").value;
         const duracao = Number(document.getElementById("duracao").value);
         const dataEstreia = document.getElementById("dataEstreia").value;
+
+        if (!titulo || !genero || !descricao || !classificacao || duracao <= 0 || !dataEstreia) {
+
+            mensagem.innerHTML = `
+            <div class="alert alert-danger mt-3">
+                Preencha todos os campos!
+            </div>
+            `
+
+            setTimeout(() => {
+                mensagem.innerHTML = "";
+            }, 3000);
+
+            return;
+        }
 
         const filme = {
             titulo,
@@ -24,6 +40,25 @@ if (btnSalvarFilme) {
         filmes.push(filme)
 
         localStorage.setItem("filmes", JSON.stringify(filmes))
+
+        document.getElementById("titulo").value = "";
+        document.getElementById("genero").value = "";
+        document.getElementById("descricao").value = "";
+        document.getElementById("classificacao").selectedIndex = 0;
+        document.getElementById("duracao").value = "";
+        document.getElementById("dataEstreia").value = "";
+
+
+        mensagem.innerHTML = `
+            <div class="alert alert-success mt-3">
+                Filme cadastrado com sucesso! 🎬
+            </div>
+        `
+
+        // Apaga mensagem de Filme cadastrado após 3 segundos
+        setTimeout(() => {
+            mensagem.innerHTML = "";
+        }, 3000);
 
     });
 
