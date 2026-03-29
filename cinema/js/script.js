b1500aac698211ab259f7f70b671294ca4e5c7a9
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-03-29 15:35:42 -0300

    corrige carregamento do script em múltiplas páginas e captura do clique no botão de salvar Filmes

---
+++ cinema/js/script.js
@@ -0,0 +1,8 @@
+const btnSalvarFilme = document.getElementById("btnSalvarFilme");
+
+if (btnSalvarFilme) {
+    btnSalvarFilme.addEventListener("click", function () {
+        console.log("Clicou em Salvar Filme")
+
+    });
+}
