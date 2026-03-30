141a5aba546cab7ec2af0c3d6bfe4f2c62e00776
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-03-30 20:19:46 -0300

    finalização de cadastro de sessões com validação completa e integração

--- cinema/js/script.js
+++ cinema/js/script.js
@@ -68,48 +68,6 @@ if (btnSalvarFilme) {
     });
 }
 
-/* 
-======================== SESSÕES ========================
-*/
-
-const selectFilme = document.getElementById("filme");
-
-if (selectFilme) {
-
-    let filmes = JSON.parse(localStorage.getItem("filmes")) || [];
-
-    selectFilme.innerHTML = '<option value="" disabled selected>Selecione um filme</option>';
-
-    filmes.forEach(function (filme, index) {
-
-        const option = document.createElement("option");
-
-        option.value = index;
-        option.textContent = `${filme.titulo} (${filme.genero})`;
-
-        selectFilme.appendChild(option);
-    });
-}
-
-const selectSala = document.getElementById("sala");
-
-if (selectSala) {
-
-    let salas = JSON.parse(localStorage.getItem("salas")) || [];
-
-    selectSala.innerHTML = '<option value="" disabled selected>Selecione uma sala</option>';
-
-    salas.forEach(function (sala, index) {
-
-        const option = document.createElement("option");
-
-        option.value = index;
-        option.textContent = `${sala.nomeSala} - ${sala.tipoSala} (${sala.capacidade} lugares)`;
-
-        selectSala.appendChild(option);
-    });
-
-}
 
 /* 
 ======================== SALAS ========================
@@ -173,3 +131,112 @@ if (btnSalvarSala) {
     })
 
 }
+
+/* 
+======================== SESSÕES ========================
+*/
+
+const selectFilme = document.getElementById("filme");
+
+if (selectFilme) {
+
+    let filmes = JSON.parse(localStorage.getItem("filmes")) || [];
+
+    selectFilme.innerHTML = '<option value="" disabled selected>Selecione um filme</option>';
+
+    filmes.forEach(function (filme, index) {
+
+        const option = document.createElement("option");
+
+        option.value = index;
+        option.textContent = `${filme.titulo} (${filme.genero})`;
+
+        selectFilme.appendChild(option);
+    });
+}
+
+const selectSala = document.getElementById("sala");
+
+if (selectSala) {
+
+    let salas = JSON.parse(localStorage.getItem("salas")) || [];
+
+    selectSala.innerHTML = '<option value="" disabled selected>Selecione uma sala</option>';
+
+    salas.forEach(function (sala, index) {
+
+        const option = document.createElement("option");
+
+        option.value = index;
+        option.textContent = `${sala.nomeSala} - ${sala.tipoSala} (${sala.capacidade} lugares)`;
+
+        selectSala.appendChild(option);
+    });
+}
+
+const btnSalvarSessao = document.getElementById("btnSalvarSessao");
+const mensagem = document.getElementById("mensagem");
+
+if (btnSalvarSessao) {
+
+    btnSalvarSessao.addEventListener("click", function () {
+
+        const filme = document.getElementById("filme").value;
+        const sala = document.getElementById("sala").value;
+        const dataSessao = document.getElementById("dataSessao").value;
+        const horarioSessao = document.getElementById("horarioSessao").value;
+        const precoIngresso = Number(document.getElementById("precoIngresso").value);
+
+        if (filme === "" || sala === "" || !dataSessao || !horarioSessao || precoIngresso <= 0) {
+
+            mensagem.innerHTML = `
+            <div class="alert alert-danger mt-3">
+                Preencha todos os campos!
+            </div>
+            `
+
+            setTimeout(() => {
+                mensagem.innerHTML = "";
+            }, 3000);
+
+            return;
+        }
+
+        let filmes = JSON.parse(localStorage.getItem("filmes")) || [];
+        let salas = JSON.parse(localStorage.getItem("salas")) || [];
+
+        // Objeto Sessão
+        const sessao = {
+            filme: filmes[filme].titulo,
+            sala: salas[sala].nomeSala,
+            dataSessao,
+            horarioSessao,
+            precoIngresso: precoIngresso.toFixed(2)
+        };
+
+        let sessoes = JSON.parse(localStorage.getItem("sessoes")) || [];
+
+        sessoes.push(sessao);
+
+        localStorage.setItem("sessoes", JSON.stringify(sessoes));
+
+        document.getElementById("filme").selectedIndex = 0;
+        document.getElementById("sala").selectedIndex = 0;
+        document.getElementById("dataSessao").value = "";
+        document.getElementById("horarioSessao").value = "";
+        document.getElementById("precoIngresso").value = "";
+        
+
+        mensagem.innerHTML = `
+            <div class="alert alert-success mt-3">
+                Sessão cadastrada com sucesso! 🍿
+            </div>
+        `
+
+        setTimeout(() => {
+            mensagem.innerHTML = "";
+        }, 3000);
+    })
+}
+
+
